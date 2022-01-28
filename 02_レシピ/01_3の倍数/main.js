c8343477d03be0f3b376a8134fe51cabfe27be32
Ryota671 <rmdmatsuda@gmail.com>
2022-01-29 02:46:30 +0900

    [発展★ - 便利なボタン]を作成しました。

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,11 +1,11 @@
 const genkiFunction = function (number) {
   for (let n = 1; n <= number; n++) {
-    if (n % 15 === 0) {
-      console.log("FizzBuzz")
-    } else if (n % 3 === 0) {
-      console.log("Fizz")
-    } else if (n % 5 === 0) {
-      console.log("Buzz")
+    if (n % 3 === 0) {
+      console.log("3が大好き!!!")
+    } else if (n + "3") {
+      console.log("3が大好き!!!")
+    } else if ("3" + n) {
+      console.log("3が大好き!!!")
     } else {
       console.log(n)
     }
